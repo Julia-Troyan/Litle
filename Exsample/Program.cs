7b55fcd1bbe838b5b2b52da361b5fa771b383671
Юлия <uliana.troyan@mail.ru>
2023-11-13 12:26:39 +0300

    первая фиксация

---
+++ Exsample/Program.cs
@@ -0,0 +1,18 @@
+﻿int countEven = 0;
+int countOdd = 0;
+Console.WriteLine( "Введите число" );
+char[] nums = Console.ReadLine().ToCharArray();
+for ( int i = 0; i < nums.Length; i++ )
+{
+    if ( int.Parse( nums[i].ToString() ) % 2 == 0 )
+    {
+        countEven++;
+    }
+    else
+    {
+        countOdd++;
+    }
+}
+
+Console.WriteLine($"{countEven} четных  \n{countOdd} нечетных");
+Console.ReadKey();
